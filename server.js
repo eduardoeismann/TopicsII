80060a56683b50dfd9a1680e7e97067e64c7fe0a
Eduardo Eismann <eduardo.eismann@gmail.com>
2019-04-13 17:57:27 -0300

    Starting the project

---
+++ server.js
@@ -0,0 +1,7 @@
+//console.log("Que a força esteja com você");
+const express = require('express');
+const app = express();
+
+app.listen( 3000, function() {
+    console.log('Server running on port 3000');
+});
