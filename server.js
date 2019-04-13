622d9648ca33a4fb6273b34b718bfb1f6914bc62
Eduardo Eismann <eduardo.eismann@gmail.com>
2019-04-13 18:10:11 -0300

    Starting communication with server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-//console.log("Que a força esteja com você");
 const express = require('express');
 const app = express();
 
+app.set( 'view engine', 'ejs' );
+
 app.listen( 3000, function() {
     console.log('Server running on port 3000');
+});
+
+app.get( '/', ( req, res ) => {
+    // res.send( 'Que a força esteja com você!' );
+    res.render( 'index.ejs' );
 });
