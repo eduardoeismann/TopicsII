8972f3e9ffab14de3847b987b063477591e44a4e
Eduardo Eismann <eduardo.eismann@gmail.com>
2019-04-15 18:08:10 -0300

    Added IP

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const ObjectId = require( 'mongodb' ).ObjectId;
 
 const uri = "mongodb+srv://eduardo:Feevale2019@topicosii-flrey.mongodb.net/test?retryWrites=true";
 
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
 MongoClient.connect(uri,{ useNewUrlParser: true },function(err,db){
 
     app.listen( 8080, () => {
