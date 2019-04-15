59d7644f573b89cd45d8ad407d50f342ef339da3
Eduardo Eismann <eduardo.eismann@gmail.com>
2019-04-15 00:47:21 -0300

    Read Done!

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 const app = express();
 
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require( 'mongodb' ).MongoClient;
 const uri = "mongodb+srv://<user>:<password>@topicosii-flrey.mongodb.net/test?retryWrites=true";
 
 MongoClient.connect( uri, ( err, client ) => {
     if ( err ) return console.log( err );
 
-    db = client.db('topicosii');
+    db = client.db( 'topicosii' );
 
     app.listen( 3000, () => {
-        console.log('Server running on port 3000');
+        console.log( 'Server running on port 3000' );
     });
 });
 
-app.use( bodyParser.urlencoded({ extended: true }) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 app.set( 'view engine', 'ejs' );
 
 app.get( '/', ( req, res ) => {
     res.render( 'index.ejs' );
 });
 
-app.get( '/', (req, res) => {
-    var cursor = db.collection('data').find();
+app.get( '/', ( req, res ) => {
+    var cursor = db.collection( 'data' ).find();
 });
 
-app.get('/', ( req, res ) => {
-    db.collection('data').find().toArray( ( err, results ) => {
+app.get( '/show', ( req, res ) => {
+    db.collection( 'data' ).find().toArray( ( err, results ) => {
         if ( err ) return console.log( err );
 
         res.render( 'show.ejs', { data: results } );
@@ -35,11 +35,10 @@ app.get('/', ( req, res ) => {
 });
 
 app.post( '/show', ( req, res ) => {
-    db.collection('data').save( req.body, ( err, result ) => {
+    db.collection( 'data' ).save( req.body, ( err, result ) => {
         if ( err ) return console.log( err );
 
-        console.log('salvo no banco de dados');
-
-        res.redirect('/show');
+        console.log( 'Dados salvos!' );
+        res.redirect( '/show' );
     });
 });
