363bd65e09c61eb81c45d7a7db645c24e4efc8e8
Eduardo Eismann <eduardo.eismann@gmail.com>
2019-04-15 18:39:53 -0300

    Connections updated!

--- server.js
+++ server.js
@@ -7,44 +7,20 @@ const ObjectId = require( 'mongodb' ).ObjectId;
 
 const uri = "mongodb+srv://eduardo:Feevale2019@topicosii-flrey.mongodb.net/test?retryWrites=true";
 
-'use strict';
+MongoClient.connect(uri,{ useNewUrlParser: true },function(err,client){
 
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
-  });
-});
-
-MongoClient.connect(uri,{ useNewUrlParser: true },function(err,db){
+    db = client.db( 'topicosii' );
 
     app.listen( 8080, () => {
         console.log( 'Server running on port 3000' );
     });
 
-  if(err){
-      console.log(err);
-  }
-  else {
-      console.log('connected to '+ uri);
-      db.close();
-  }
+    if(err){
+        console.log(err);
+    } else {
+        console.log('connected to '+ uri);
+//      db.close();
+    }
 });
 
 app.use( bodyParser.urlencoded( { extended: true } ) );
@@ -93,7 +69,8 @@ app.route( '/edit/:id' ).get( ( req, res ) => {
     db.collection( 'data' ).updateOne( {_id: ObjectId( id ) }, {
         $set: {
             name: name,
-            surname: surname
+            surname: surname,
+            file: file
         }
     }, ( err, result ) => {
         if ( err ) return res.send( err );
